d88ee17307ca42cb54fe096ceaf1f7e7325e54ab
Matt Burke <spraints@gmail.com>
2013-03-07 10:16:58 -0500

    Initial program.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        class Thing
+        {
+            public string Ok { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Dump<Thing>(x => x.Ok);
+        }
+
+        static void Dump<X>(Expression<Func<X, string>> e)
+        {
+        }
+    }
+}
