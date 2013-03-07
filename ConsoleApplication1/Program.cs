cb36323671fc88172a1aee3593f95d914cb39b15
Matt Burke <spraints@gmail.com>
2013-03-07 10:35:58 -0500

    Augment, but not how it should be.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,16 +12,31 @@ namespace ConsoleApplication1
 
         static void Main(string[] args)
         {
-            Dump<Thing, string>(x => x.Ok);
+            var f = F<Thing, string>(x => x.Ok);
+            Dump(f);
+            var g = Augment(f);
+            Dump(g);
+        }
+
+        static Expression<Func<X, bool>> Augment<X>(Expression<Func<X, string>> e)
+        {
+            return x => e.Compile()(x).Contains("ok");
         }
 
         static void Dump<X, Y>(Expression<Func<X, Y>> e)
         {
+            Console.WriteLine("-----");
+            Console.WriteLine(e);
             Console.WriteLine(e.Type);
             Console.WriteLine(e.Parameters);
             Console.WriteLine(e.NodeType);
             Console.WriteLine(e.Body);
-            Console.WriteLine(e);
+        }
+
+        // This seemed like the easiest way to do `var f = x => x.Ok`.
+        static new Expression<Func<X, Y>> F<X, Y>(Expression<Func<X, Y>> f)
+        {
+            return f;
         }
     }
 }
